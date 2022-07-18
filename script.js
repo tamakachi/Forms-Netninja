f812e1ae20a346c4d7bcbd80aefa184138d53683
Kent Van Heerden <kenttechnical@gmail.com>
2022-07-18 13:00:51 +0200

    Basic form with submit
    Uses event handler to access values from form fields
    Overrides default refresh action of submit button

---
+++ script.js
@@ -0,0 +1,17 @@
+// When creating an event listener for submit buttons
+// We place the event listener on the form
+// Not on the button itself
+// The default action of the submit button is to refresh the page
+
+
+const form = document.querySelector(".signup-form")
+
+form.addEventListener("submit",e =>{
+
+    e.preventDefault() // prevents refresh default action
+    //quick reference to value using dot notation, quicker than creating a selector
+    console.log(e.username.value)
+    // e.username refers to an ID or Name property called username
+    // It returns that value
+
+})
